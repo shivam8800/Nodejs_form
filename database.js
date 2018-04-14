2c7fe2052c19e4bbd780036262c2d843ca963257
shivam8800 <shivam16@navgurukul.org>
2018-04-14 13:25:00 +0530

    done backend of authentication

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 // Include Mongoose ORM to connect with database
 const Mongoose = require('mongoose')
 // Making connection with `hasjobdb` database in your local machine
-Mongoose.connect('mongodb://localhost/form/');
+Mongoose.connect('mongodb://devteam:teamdev@ds111059.mlab.com:11059/birch-dev');
 //connect database
 var db = Mongoose.connection;
 //this is will happen when we got an errror while connecting with database
